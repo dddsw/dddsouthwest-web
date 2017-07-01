b7d21b988a98074a0aaa4d8dcfa1657138e00c75
Joseph Woodward <joseph.woodward@xeuse.com>
2017-07-01 01:40:02 +0100

    Added identity server

--- src/DDDSouthWest.Web/Startup.cs
+++ src/DDDSouthWest.Web/Startup.cs
@@ -33,6 +33,7 @@ namespace DDDSouthWest.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
