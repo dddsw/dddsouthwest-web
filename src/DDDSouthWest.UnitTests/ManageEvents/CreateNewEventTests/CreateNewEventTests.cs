becea66029f0747add9fa4b9adfde3d0c6883ed3
Joseph Woodward <joseph.woodward@xeuse.com>
2017-07-28 03:23:35 +0100

    Unit tests added

---
+++ src/DDDSouthWest.UnitTests/ManageEvents/CreateNewEventTests/CreateNewEventTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using DDDSouthWest.Domain;
+using DDDSouthWest.Domain.Features.Account.ManageEvents.CreateNewEvent;
+using Shouldly;
+using Xunit;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace DDDSouthWest.UnitTests.ManageEvents.CreateNewEventTests
+{
+    public class CreateNewEventTests
+    {
+        [Fact]
+        public async Task Create_new_event()
+        {
+            var mediator = ResolveContainer.MediatR;
+
+            var result = await mediator.Send(new CreateNewEvent.Command
+            {
+                EventDate = DateTime.Now,
+                EventFilename = Guid.NewGuid().ToString(),
+                EventName = "Event name"
+            });
+
+            result.Id.ShouldBeGreaterThan(0);
+        }
+        
+        [Fact]
+        public async Task Prevent_duplicate_event()
+        {
+            var mediator = ResolveContainer.MediatR;
+
+            var command = new CreateNewEvent.Command
+            {
+                EventDate = DateTime.Now,
+                EventFilename = Guid.NewGuid().ToString(),
+                EventName = "Event name"
+            };
+
+            mediator.Send(command).Wait();
+
+            await Should.ThrowAsync<DuplicateRecordException>(mediator.Send(command));
+        }
+
+        [Fact]
+        public async Task Prevent_incomplete_event_submissions()
+        {
+            var mediator = ResolveContainer.MediatR;
+
+            var invalidEvent = new CreateNewEvent.Command
+            {
+                EventDate = DateTime.Now
+            };
+            
+            await Should.ThrowAsync<ValidationException>(mediator.Send(invalidEvent));
+        }
+    }
+}
---
+++ src/DDDSouthWest.UnitTests/ResolveContainer.cs
@@ -0,0 +1,45 @@
+﻿using DDDSouthWest.Domain;
+using DDDSouthWest.Domain.Features.Public.Page;
+using MediatR;
+using StructureMap;
+
+namespace DDDSouthWest.UnitTests
+{
+    public static class ResolveContainer
+    {
+        public static IMediator MediatR
+        {
+            get
+            {
+                var container = new Container(cfg =>
+                {
+                    cfg.Scan(scanner =>
+                    {
+                        scanner.AssemblyContainingType<GetPage.Query>();
+                        scanner.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<>));
+                        scanner.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>));
+                        scanner.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<>));
+                        scanner.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<,>));
+                        scanner.ConnectImplementationsToTypesClosing(typeof(INotificationHandler<>));
+                        scanner.ConnectImplementationsToTypesClosing(typeof(IAsyncNotificationHandler<>));
+                    });
+
+                    cfg.For<ClientConfigurationOptions>().Use(() => new ClientConfigurationOptions
+                    {
+                        Database = new Database
+                        {
+                            ConnectionString =
+                                "Host=localhost;Username=dddsouthwest_user;Password=letmein;Database=dddsouthwest"
+                        }
+                    }).Singleton();
+                    
+                    cfg.For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+                    cfg.For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+                    cfg.For<IMediator>().Use<Mediator>();
+                });
+
+                return container.GetInstance<IMediator>();
+            }
+        }
+    }
+}
