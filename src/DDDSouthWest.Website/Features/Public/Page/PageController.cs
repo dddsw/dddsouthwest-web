9684fea0f01d443e257e3c2e2f207e8235421d26
stuart.lang@just-eat.com <stuart.lang@just-eat.com>
2021-12-15 01:58:45 +0000

    Try to fix fixed pages

--- src/DDDSouthWest.Website/Features/Public/Page/PageController.cs
+++ src/DDDSouthWest.Website/Features/Public/Page/PageController.cs
@@ -1,26 +1,28 @@
-﻿using System.IO;
-using System.Net;
+﻿using System.Net;
 using System.Threading.Tasks;
 using DDDSouthWest.Domain;
 using DDDSouthWest.Domain.Features.Public.Page;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 
 namespace DDDSouthWest.Website.Features.Public.Page
 {
     public class PageController : Controller
     {
         private readonly IMediator _mediator;
+        private readonly ICompositeViewEngine _compositeViewEngine;
 
-        public PageController(IMediator mediator)
+        public PageController(IMediator mediator, ICompositeViewEngine compositeViewEngine)
         {
             _mediator = mediator;
+            _compositeViewEngine = compositeViewEngine;
         }
         
         public async Task<IActionResult> Index(GetPage.Query query)
         {
-            string path = $"./Features/Public/Page/{query.Filename}.cshtml";
-            if (System.IO.File.Exists(path))
+            string path = $"~/Features/Public/Page/{query.Filename}.cshtml";
+            if (_compositeViewEngine.GetView("", path, false).Success)
             {
                 return View(path);   
             }
