3e6b994821c51efea439fc89f298e66cd3b5c180
Hannah Price <hannahprice94@hotmail.co.uk>
2022-05-14 22:10:14 +0100

    Updated sponsors page

---
+++ src/DDDSouthWest.Website/Features/Public/Sponsors/SponsorsController.cs
@@ -0,0 +1,14 @@
+﻿using DDDSouthWest.Website.Framework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDDSouthWest.Website.Features.Public.Sponsors
+{
+    public class SponsorsController : Controller
+    {
+        [Route("/sponsors/", Name = RouteNames.Sponsors)]
+        public IActionResult Index()
+        {
+            return View(new SponsorsViewModel());
+        }
+    }
+}
